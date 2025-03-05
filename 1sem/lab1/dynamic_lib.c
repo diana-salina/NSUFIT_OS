4693b13dd2b20de0adebe43fc982d4c375ab6eb5
diana-salina <dianasalina@yandex.ru>
2025-03-05 12:37:10 +0700

    lab1

---
+++ 1sem/lab1/dynamic_lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "dynamic_lib.h"
+
+void hello_from_dynamic_lib() {
+    printf("Hello from dynamic library!\n");
+}
---
+++ 1sem/lab1/hello2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello_static.h"
+
+int main(void) {
+	hello_from_static_lib();
+	return 0;
+}
